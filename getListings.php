32cf5d0d9e4cd8d02f2d6ff45850806b2e508d83
William Hankins <75007804+whankinsiv@users.noreply.github.com>
2022-12-09 13:38:07 -0700

    Update getListings.php

--- getListings.php
+++ getListings.php
@@ -1,10 +1,11 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$database = "u658727581_MLSDatabase";
+$username = "u658727581_MLSUser";
+$password = "Project1234";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
