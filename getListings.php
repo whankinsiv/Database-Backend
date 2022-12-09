3b5ae3ea1dec33c5d6a8c9386b1bcef680bc179d
William Hankins <75007804+whankinsiv@users.noreply.github.com>
2022-12-09 16:46:13 -0700

    Update getListings.php

--- getListings.php
+++ getListings.php
@@ -12,22 +12,18 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 // Get the current listing id's and asking prices
-$query = "SELECT house_id, asking_price FROM current_listings";
-$result = $conn->query($query);
+$query1 = "SELECT house_id, asking_price FROM current_listings";
+$result1 = $conn->query($query1);
 
 // Process the result
-while ($row = $result->fetch_array()) {
-  $data[] = $row;
-}
-
-$i = 0;
-
-// Get property address, city, state using house_id
-while($i < count($data) {
-  $query = "SELECT street_address, city, State FROM house where house_id = '" . $data[i] . "'";
-  $result = mysqli_query($conn, $query);
-  echo $result;
-  $i++;
+while ($row1 = $result1->fetch_array()) {
+  $query2 = "SELECT street_address, city, State FROM house where house_id = '" . $row1["house_id"]] . "'";
+  $result2 = mysqli_query($conn, $query2);
+  
+  // Process result of second query
+  while ($row2 = mysqli_fetch_array($result2)) {
+    $data[] = $row2;
+  }
 }
 echo json_encode($data);
 
