2e6427239486f1393d401368a877459c6cc893e2
William Hankins <75007804+whankinsiv@users.noreply.github.com>
2022-12-09 21:05:54 -0700

    Update showListing.js

--- showListing.js
+++ showListing.js
@@ -24,7 +24,7 @@ async function listingClicked(house_id) {
     
     // Populate showingListingPopupHeader
     listingPopupHeader.innerHTML = "<div>" + address + "<\div>";
-    listingPopupHeader.innerHTML += "<div>" + askingPrice + "<\div>";
+    listingPopupHeader.innerHTML += "<div>$" + askingPrice + "<\div>";
     
     // Populate showingListingPopupBody
     listingPopupBody.innerHTML = "<div>Bedrooms: " + bedroomCount + "<\div>";
