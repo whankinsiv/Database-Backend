ea96d27d528f0983021b6af4ebb68222706cb65e
William Hankins <75007804+whankinsiv@users.noreply.github.com>
2022-12-10 13:02:08 -0700

    Update showListing.js

--- showListing.js
+++ showListing.js
@@ -45,8 +45,8 @@ async function listingClicked(house_id) {
     listingPopupHistory.innerHTML += "<div>Put past listings w/ same house id here<\div>";
 
     // Populate footer
-    listingPopupFooter.innerHTML = "<div>List Date: " + listDate + "</div>";
-    listingPopupFooter.innerHTML += "<div>Listed by " + listAgent + " of " + listBrokerage + "</div>";
+    listingPopupFooter.innerHTML = "<div>Listed " + listDate + "</div>";
+    listingPopupFooter.innerHTML += "<div>Listed " + listDate + " by " + listAgent + " of " + listBrokerage + "</div>";
     
     // Make Popup visable
     listingPopup.classList.add('active');
