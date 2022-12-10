10e10b3ad23f289d8fb0214ec36ce2a43a9b1493
William Hankins <75007804+whankinsiv@users.noreply.github.com>
2022-12-09 20:12:23 -0700

    Update showListing.js

--- showListing.js
+++ showListing.js
@@ -12,6 +12,7 @@ function listingClicked(house_id) {
 
 function getListingData(house_id) {
     var script = "/getListingInfo.php?house_id=" + encodeURIComponent(house_id);
+    console.log(script);
     fetch(script)
     .then(response => response.json())
     .then(data => {
