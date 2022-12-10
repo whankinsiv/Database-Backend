0ad07cdf33d0aec32399e7c7df4602b587553965
William Hankins <75007804+whankinsiv@users.noreply.github.com>
2022-12-09 19:35:25 -0700

    Update showListing.js

--- showListing.js
+++ showListing.js
@@ -2,9 +2,29 @@ const listingPopup = document.getElementById("showListingPopup");
 
 // Function for when an individual listing is clicked.
 function listingClicked(house_id) {
+    
+    // Get listing information from the database using the house_id
+    
+    
     listingPopup.classList.add('active');
     overlay.classList.add('active');
 }
+
+function getListingData(house_id) {
+    var script = "'/getListingInfo.php?houseid=" + house_id "'"
+    fetch(script)
+    .then(response => response.json())
+    .then(data => {
+    
+    // Set Global variable for all current listings. 
+    currentListingsOrdered = data;
+    console.log(currentListingsOrdered);
+
+}
+
+
+
+
 
 function closeShowListingPopup() {
     listingPopup.classList.remove('active');
