ffa913164f9a344a1621b73bc375b25c832e6344
William Hankins <75007804+whankinsiv@users.noreply.github.com>
2022-12-09 20:10:37 -0700

    Update getListingInfo.php

--- getListingInfo.php
+++ getListingInfo.php
@@ -14,6 +14,7 @@ if ($conn->connect_error) {
 
 // Get house_id variable from JS
 $house_id = $_GET["house_id"];
+echo $house_id;
 
 // Get the listing information from the house_id
 $query = "SELECT current_listings.house_id, asking_price, street_address, city, State FROM current_listings INNER JOIN house ON current_listings.house_id = house.house_id";
