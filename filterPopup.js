83d1824aedf618f02e9b21b3164e6d1606061151
William Hankins <75007804+whankinsiv@users.noreply.github.com>
2022-12-11 15:54:32 -0700

    Update filterPopup.js

--- filterPopup.js
+++ filterPopup.js
@@ -1,33 +1,42 @@
 const filterButton = document.getElementById("filterButton")
+const setFilterButton = document.getElementById("setFilter")
+const resetFilterButton = document.getElementById("resetFilter")
+const overlay = document.getElementById('overlay')
+const filterPopup = document.getElementById('filterPopup')
+
 filterButton.addEventListener('click', () => {
     openFilterPopup()
 })
 
-const overlay = document.getElementById('overlay')
-const filterPopup = document.getElementById('filterPopup')
-function openFilterPopup() {
-    filterPopup.classList.add('active')
-    overlay.classList.add('active')
-}
-
-const setFilterButton = document.getElementById("setFilter")
-/* The table needs to be reloaded on this event */
-setFilterButton.addEventListener('click', () => {
-    closeFilterPopup()
-})
-
-function closeFilterPopup() {
-    filterPopup.classList.remove('active')
-    overlay.classList.remove('active')
-}
-
-const resetFilterButton = document.getElementById("resetFilter")
 /* All filter parameters must be reset on this event */
 resetFilterButton.addEventListener('click', () => {
     resetFilter()
 })
 
-/* Function to reset filter parameters */
+setFilterButton.addEventListener('click', () => {
+    closeFilterPopup()
+})
+
+// Function to produce reduced data array based on filter parameters 
+function setFilter() {
+    
+}
+
+// Function to reset filter parameters
 function resetFilter() {
 
 }
+
+// Function to open popup when filter button is pushed
+function openFilterPopup() {
+    filterPopup.classList.add('active')
+    overlay.classList.add('active')
+}
+
+// Function to close the popup
+function closeFilterPopup() {
+    setFilterPopup()
+    filterPopup.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
