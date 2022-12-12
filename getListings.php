c1f1992189df8de71dc0540633e28da888a4eed9
William Hankins <75007804+whankinsiv@users.noreply.github.com>
2022-12-11 17:45:55 -0700

    Update getListings.php

--- getListings.php
+++ getListings.php
@@ -12,7 +12,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 // Get the current listing id's and asking prices
-$query = "SELECT current_listings.house_id, asking_price, street_address, city, State FROM current_listings INNER JOIN house ON current_listings.house_id = house.house_id";
+$query = "SELECT current_listings.house_id, asking_price, street_address, city, State, bedroom_count, bathroom_count FROM current_listings INNER JOIN house ON current_listings.house_id = house.house_id";
 $result = $conn->query($query);
 
 // Process the result
