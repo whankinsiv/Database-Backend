e423b9eaac113c2853b32d5cab654dfcd2c63630
William Hankins <75007804+whankinsiv@users.noreply.github.com>
2022-12-13 22:45:48 -0700

    Update submitFourm.php

--- submitFourm.php
+++ submitFourm.php
@@ -12,12 +12,14 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$house_id = $_REQUEST["agent_name"];
-$house_id = $_REQUEST["licence_reference_number"];
-$house_id = $_REQUEST["firm_named"];
+$agent_name = $_REQUEST["agent_name"];
+$licence_reference_number = $_REQUEST["licence_reference_number"];
+$firm_name = $_REQUEST["firm_name"];
 
-$sql = "INSERT INTO `house` (`house_id`,`city`,`State`,`zip_code`,`street_address`,`bedroom_count`,`bathroom_count`,`square_feet`,`nearest_school`,`heating_type`,`cooling_type`,`pool`,`detached_house`,`street_parking`,`groundwater_access`,`solar_installed`,`garage_size`)
-VALUES ()";
+$sql = "INSERT INTO `agent` (`agent_name`,`licence_reference_number`,`firm_name`)
+VALUES (`" . $agent_name   "`, `" . $licence_reference_number . "`, `" . $firm_name . "`)";
+//$sql = "INSERT INTO `house` (`house_id`,`city`,`State`,`zip_code`,`street_address`,`bedroom_count`,`bathroom_count`,`square_feet`,`nearest_school`,`heating_type`,`cooling_type`,`pool`,`detached_house`,`street_parking`,`groundwater_access`,`solar_installed`,`garage_size`)
+//VALUES ()";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
