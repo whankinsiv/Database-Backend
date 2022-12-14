3d570c1f75aed6d426539e6c075a11f8f3501c3e
SAwesome1018 <54332711+SAwesome1018@users.noreply.github.com>
2022-12-13 22:25:38 -0700

    Update submitFourm.php

--- submitFourm.php
+++ submitFourm.php
@@ -1,15 +1,29 @@
 <?php
-$host = "localhost";
-$username = "username";
-$password = "";
-$dbname = "dbname";
+$servername = "localhost";
+$database = "u658727581_MLSDatabase";
+$username = "u658727581_MLSUser";
+$password = "Project1234";
 
-$conn = new mysqli($host, $username, $password, $dbname);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
 
+// Check connection
 if ($conn->connect_error) {
-    die("connection failed: " . $conn->connect_error);
-} else {
-    echo("Connected successfully\n");
+  die("Connection failed: " . $conn->connect_error);
 }
+
+
+
+$sql = "INSERT INTO `house` (`house_id`,`city`,`State`,`zip_code`,`street_address`,`bedroom_count`,`bathroom_count`,`square_feet`,`nearest_school`,`heating_type`,`cooling_type`,`pool`,`detached_house`,`street_parking`,`groundwater_access`,`solar_installed`,`garage_size`)
+VALUES ()";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Close the connection
+mysqli_close($conn);
 
 ?>
